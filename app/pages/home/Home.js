ee1335ae2b4752e8431a06bc40f1ded245f76760
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-02-13 14:19:14 -0300

    feat: projetos de lei aprovados por vereador

---
+++ app/pages/home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Progress } from '../../components/ui/Progress';
+import { useMethodWithState } from '../../hooks/useMethodWithState';
+
+export function Home() {
+  const [data] = useMethodWithState({ method: 'ProjetosDeLei.aprovados' });
+
+  return (
+    <div>
+      <div className="self-center">
+        <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl">
+          Projetos de Lei aprovados por Vereador
+        </h2>
+      </div>
+      <div className="mt-8 grid grid-cols-5 gap-4 md:grid-cols-7">
+        {data &&
+          data.map((item) => (
+            <React.Fragment key={item.author}>
+              <h3 className="col-span-2 items-center md:col-span-1">
+                {item.author}
+              </h3>
+              <Progress
+                className="col-span-2 md:col-span-5"
+                value={item.value}
+                max={data[0]?.value}
+              />
+              <h3 className="col-span-1">{item.value}</h3>
+            </React.Fragment>
+          ))}
+      </div>
+    </div>
+  );
+}
