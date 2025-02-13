fe42a23f3d4366c8a804869ef621600968cbe9d2
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-02-13 14:20:04 -0300

    chore: import new collection

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-
+import '../app/api/projetosDeLei';
 
 Meteor.startup(async () => {
-  console.log("Starting up the project");
+  console.log('Starting application');
 });
