e6746b565beeee0d9170313799c1a5d29fc78352
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-02-27 23:52:52 -0300

    style: sidebar improvements

--- app/components/ui/NavBarCaption.js
+++ app/components/ui/NavBarCaption.js
@@ -1,26 +1,26 @@
-import { GalleryVerticalEnd } from 'lucide-react';
 import * as React from 'react';
 
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './Sidebar';
-import { useNavigate } from 'react-router-dom';
 import { RoutePaths } from '../../general/RoutePaths';
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './Sidebar';
 
-export function NavBarCaption() {
-  const navigate = useNavigate();
+export function NavBarCaption({ handleItemClick }) {
+  function handleRedirect() {
+    handleItemClick({ group: 'Home', url: RoutePaths.APP });
+  }
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton
-          size="lg"
+          size="xl"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+          <div className="flex aspect-square size-12 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Bras%C3%A3o-Belo-Horizonte.svg/1200px-Bras%C3%A3o-Belo-Horizonte.svg.png" />
           </div>
           <div
             className="flex cursor-pointer flex-col gap-0.5 leading-none"
-            onClick={() => navigate(RoutePaths.APP)}
+            onClick={handleRedirect}
           >
             <span className="font-semibold">Câmara Municipal</span>
             <span className="">Belo Horizonte - MG</span>
