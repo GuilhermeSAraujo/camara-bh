9c668462a246c4069cf6e4e1cd2a65b6b317af3a
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-04-08 00:31:46 -0300

    feat: refactor App component to use HashRouter and lazy loading for routes

--- app/general/App.js
+++ app/general/App.js
@@ -1,18 +1,40 @@
-import React, { Suspense } from 'react';
-import { RouterProvider } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
-
-import { router } from './Router';
-
+import { Layout } from '../components/Layout';
+import { RoutePaths } from './RoutePaths';
 import { Loading } from '../components/Loading';
+
+const Home = lazy(() => import('../pages/home/Home'));
+const ProjetosDeLei = lazy(
+  () => import('../pages/projetosDeLei/ProjetosDeLei')
+);
+const ProjetosDeLeiPartidos = lazy(
+  () => import('../pages/projetosDeLeiPartidos/ProjetosDeLeiPartidos')
+);
+const Vereadores = lazy(() => import('../pages/vereadores/Vereadores'));
 
 export function App() {
   return (
     <>
-      <RouterProvider router={router}>
-        <Suspense fallback={<Loading name="suspense" />} />
-      </RouterProvider>
-
+      <HashRouter>
+        <Suspense fallback={<Loading name="suspense" />}>
+          <Routes>
+            <Route path={RoutePaths.APP} element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route
+                path={RoutePaths.PROJETOS_DE_LEI}
+                element={<ProjetosDeLei />}
+              />
+              <Route
+                path={RoutePaths.PROJETOS_DE_LEI_POR_PARTIDOS}
+                element={<ProjetosDeLeiPartidos />}
+              />
+              <Route path={RoutePaths.VEREADORES} element={<Vereadores />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </HashRouter>
       <Toaster />
     </>
   );
