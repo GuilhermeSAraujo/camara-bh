a65bb09af55dde9b1e135a02106b831dc88557fe
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-04-08 11:58:53 -0300

    feat: implement preload for critical components using requestIdleCallback

--- client/main.js
+++ client/main.js
@@ -3,7 +3,33 @@ import { Meteor } from 'meteor/meteor';
 import { createRoot } from 'react-dom/client';
 import { App } from '../app/general/App';
 
+const preloadCriticalComponents = () => {
+  const criticalComponents = [
+    '../app/pages/projetosDeLei/ProjetosDeLeiList',
+    '../app/components/ui/ChartDetailsModal',
+  ];
+
+  // Inicia o preload após o carregamento da página
+  if ('requestIdleCallback' in window) {
+    window.requestIdleCallback(() => {
+      criticalComponents.forEach((component) => {
+        import(component);
+      });
+    });
+  } else {
+    // Fallback para browsers que não suportam requestIdleCallback
+    setTimeout(() => {
+      criticalComponents.forEach((component) => {
+        import(component);
+      });
+    }, 1000);
+  }
+};
+
 Meteor.startup(() => {
+  // Inicia o preload
+  preloadCriticalComponents();
+
   const root = createRoot(document.getElementById('app'));
   root.render(
     <StrictMode>
