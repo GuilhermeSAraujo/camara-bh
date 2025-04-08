1f2e0282ad6761fb475c789345dd2d5d0a312295
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2025-04-08 11:01:35 -0300

    feat: refactor Router to use React.lazy for component imports to enhance code organization

--- app/general/Router.js
+++ app/general/Router.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import { RoutePaths } from './RoutePaths';
 
-// const Home = React.lazy(() => import('../pages/home/Home'));
-// const Layout = React.lazy(() => import('../components/Layout'));
-// const ProjetosDeLei = React.lazy(
-//   () => import('../pages/projetosDeLei/ProjetosDeLei')
-// );
-// const ProjetosDeLeiPartidos = React.lazy(
-//   () => import('../pages/projetosDeLeiPartidos/ProjetosDeLeiPartidos')
-// );
-// const Vereadores = React.lazy(() => import('../pages/vereadores/Vereadores'));
-
-import Home from '../pages/home/Home';
-import Layout from '../components/Layout';
-import ProjetosDeLei from '../pages/projetosDeLei/ProjetosDeLei';
-import ProjetosDeLeiPartidos from '../pages/projetosDeLeiPartidos/ProjetosDeLeiPartidos';
-import Vereadores from '../pages/vereadores/Vereadores';
+const Home = React.lazy(() => import('../pages/home/Home'));
+const Layout = React.lazy(() => import('../components/Layout'));
+const ProjetosDeLei = React.lazy(
+  () => import('../pages/projetosDeLei/ProjetosDeLei')
+);
+const ProjetosDeLeiPartidos = React.lazy(
+  () => import('../pages/projetosDeLeiPartidos/ProjetosDeLeiPartidos')
+);
+const Vereadores = React.lazy(() => import('../pages/vereadores/Vereadores'));
 
 export const router = createBrowserRouter([
   {
