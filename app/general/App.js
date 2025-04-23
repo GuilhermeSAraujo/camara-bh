7bab48d95124d1c21c4695d5836363c864ad78ba
Guilherme Souza <guilhermesouzae21@gmail.com>
2025-04-22 23:49:22 -0300

    feat: implement Textarea and Toaster components with theming support

--- app/general/App.js
+++ app/general/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { Toaster } from 'sonner';
+import { Toaster } from '../components/ui/Toaster';
 import { router } from './Router';
 import { Loading } from '../components/Loading';
 
